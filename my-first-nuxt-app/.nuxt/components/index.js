bb2c9ce65e919be700be695f530ea261a2502bf0
Jamil Nawaz <jamilnawaz88@gmail.com>
2020-09-16 09:21:36 +0100

    Added navigation components and added header to default layout so its visible across all.

--- my-first-nuxt-app/.nuxt/components/index.js
+++ my-first-nuxt-app/.nuxt/components/index.js
@@ -1,3 +1,9 @@
 export { default as PostPreview } from '../../components/Posts/PostPreview.vue'
+export { default as TheHeader } from '../../components/Navigation/TheHeader.vue'
+export { default as TheSideNavToggle } from '../../components/Navigation/TheSideNavToggle.vue'
+export { default as TheSidenav } from '../../components/Navigation/TheSidenav.vue'
 
 export const LazyPostPreview = import('../../components/Posts/PostPreview.vue' /* webpackChunkName: "components/Posts/PostPreview" */).then(c => c.default || c)
+export const LazyTheHeader = import('../../components/Navigation/TheHeader.vue' /* webpackChunkName: "components/Navigation/TheHeader" */).then(c => c.default || c)
+export const LazyTheSideNavToggle = import('../../components/Navigation/TheSideNavToggle.vue' /* webpackChunkName: "components/Navigation/TheSideNavToggle" */).then(c => c.default || c)
+export const LazyTheSidenav = import('../../components/Navigation/TheSidenav.vue' /* webpackChunkName: "components/Navigation/TheSidenav" */).then(c => c.default || c)
