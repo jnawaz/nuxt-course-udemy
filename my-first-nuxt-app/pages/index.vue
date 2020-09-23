c6af984540aaced7dc02f43a96eaeb2bfbc51067
Jamil Nawaz <jamilnawaz88@gmail.com>
2020-09-23 15:44:37 +0100

    async data

--- my-first-nuxt-app/pages/index.vue
+++ my-first-nuxt-app/pages/index.vue
@@ -14,13 +14,25 @@ export default {
   components: {
     PostList: PostList
   },
-  data() {
-    return {
-      loadedPosts: [
-        {id: 1, title: "My first post", previewText: "My first post preview", thumbnail: "https://static.pexels.com/photos/270348/pexels-photo-270348.jpeg"},
-        {id: 2, title: "My second post", previewText: "My second post preview", thumbnail: "https://static.pexels.com/photos/270348/pexels-photo-270348.jpeg"}
-        ]
-    }
+  asyncData(context, callback) { // executed on the server and waits for task to finish and only calls in pages
+    setTimeout(() => {
+      callback(null, {
+loadedPosts: [
+        {id: '1', title: "My first post", previewText: "My first post preview", thumbnail: "https://static.pexels.com/photos/270348/pexels-photo-270348.jpeg"},
+        {id: '2', title: "My second post", previewText: "My second post preview", thumbnail: "https://static.pexels.com/photos/270348/pexels-photo-270348.jpeg"}
+      ]
+      });
+
+    }, 1500);
+    },
+  // data() {
+  //   return {
+  //     loadedPosts: []
+  //   }
+  // },
+  created() {
+    //happens on client and not the server side
+
   }
 }
 </script>
